393db369d8269689ed632c0b8154190c73ec9340
Adrian Bay Dorph <32704186+adriandorph@users.noreply.github.com>
2022-10-27 11:59:33 +0200

    lec07 launch.sh

--- launch.sh
+++ launch.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
-cd Lectures/Lec04/ 
+cd Lectures/Lec07/Microc 
 alias fslex="mono /Users/adrian/Documents/ITU/5_Semester/Programmer\ som\ data/PRDAT/fsharp/FsLex.exe --unicode"
 alias fsyacc="mono /Users/adrian/Documents/ITU/5_Semester/Programmer\ som\ data/PRDAT/fsharp/fsyacc.exe"
-rm -r FunPar.fs FunLex.fs
-fsyacc --module FunPar FunPar.fsy
-fslex --unicode FunLex.fsl
-fsharpi -r ../../fsharp/FsLexYacc.Runtime.dll Fun/Absyn.fs Fun/FunPar.fs Fun/FunLex.fs Fun/Parse.fs TypedFun/TypedFun.fs
+rm -r CPar.fs CLex.fs
+fslex --unicode CLex.fsl
+fsyacc --module CPar CPar.fsy
+fsharpi -r FsLexYacc.Runtime.dll Absyn.fs CPar.fs CLex.fs Parse.fs LeMachine.fs Comp.fs ParseAndComp.fs   
 
 
 
